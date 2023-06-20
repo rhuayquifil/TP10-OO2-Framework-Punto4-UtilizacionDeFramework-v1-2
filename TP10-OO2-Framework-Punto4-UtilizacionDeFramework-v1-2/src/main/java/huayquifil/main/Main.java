5fefd8b7d92a599286a68127bfc782292c2bdf86
rhuayquifil <ezehuayquifil@hotmail.com>
2023-06-20 02:27:39 -0300

    utilizacion de framework-v1.2

---
+++ TP10-OO2-Framework-Punto4-UtilizacionDeFramework-v1-2/src/main/java/huayquifil/main/Main.java
@@ -0,0 +1,15 @@
+package huayquifil.main;
+
+import huayquifil.framework.AlzarAccionesDeArchivo;
+import huayquifil.framework.Framework;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Framework framework = new Framework(new AlzarAccionesDeArchivo(
+				"C:\\Users\\ezehu\\git\\TP10-OO2-Framework-v0-0\\TP10-OO2-Framework-v0-0\\src\\main\\resources\\acciones.json"));
+		framework.init();
+	}
+
+}
