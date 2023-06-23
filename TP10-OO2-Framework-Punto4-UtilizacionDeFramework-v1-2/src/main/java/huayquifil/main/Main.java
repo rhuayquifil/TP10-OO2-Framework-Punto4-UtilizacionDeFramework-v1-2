d4880760f6d7379749408cda145ae28d870f2429
rhuayquifil <ezehuayquifil@hotmail.com>
2023-06-22 23:00:37 -0300

    implementacion de nueva accion y prueba

--- TP10-OO2-Framework-Punto4-UtilizacionDeFramework-v1-2/src/main/java/huayquifil/main/Main.java
+++ TP10-OO2-Framework-Punto4-UtilizacionDeFramework-v1-2/src/main/java/huayquifil/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		Framework framework = new Framework(new AlzarAccionesDeArchivo(
-				"C:\\Users\\ezehu\\git\\TP10-OO2-Framework-v0-0\\TP10-OO2-Framework-v0-0\\src\\main\\resources\\acciones.json"));
+				"C:\\Users\\ezehu\\git\\TP10-OO2-Framework-Punto4-UtilizacionDeFramework-v1-2\\TP10-OO2-Framework-Punto4-UtilizacionDeFramework-v1-2\\src\\main\\resources\\acciones.json"));
 		framework.init();
 	}
 
